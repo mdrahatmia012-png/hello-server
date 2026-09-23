74e4331f4dce4c66e24923b7f959400eaa28d6af
mdrahatmia012-png <mdrahatmia012@gmail.com>
2026-09-23 15:04:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from my server!"
+def home():
+    return render_template("index.html")
+
+@app.route("/message", methods=["POST"])
+def message():
+    data = request.get_json()
+    msg = data["message"]
+
+    return {
+        "reply": f"Server received: {msg}"
+    }
